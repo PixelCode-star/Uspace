1cf8b3765007595496aa1dc595d937d50705f958
pixelcode-star <cold2733@gmail.com>
2026-04-10 23:40:01 +0200

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { readFileSync } from 'node:fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    __APP_VERSION__: JSON.stringify(pkg.version ?? '0.0.0'),
+    __APP_VERSION__: JSON.stringify('1.0.0'),
   },
 })
